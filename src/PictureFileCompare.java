b0621a4cbd279afc83f12368e91bfe59b3d27276
Dave <Hobbesdcc@gmail.com>
2018-06-08 14:15:03 -0400

    Loop Feature, While loop added
     Untested with picture files, worked with directories

--- src/PictureFileCompare.java
+++ src/PictureFileCompare.java
@@ -17,13 +17,18 @@ public class PictureFileCompare {
 
 	static Scanner scan = new Scanner(System.in); 
 	
+	//Main Loop Feature
+	private static boolean mainLoop = true;
+	private static String tempString;
+	static Scanner  consolescan = new Scanner(System.in); 
+	
 	//Constructor For "PictureFileCompare" Class
 	public PictureFileCompare() // Constructor
 	{
 		// Version Number!
-		// versionNumber ="v1.10 (2015-10-26)";
+		// versionNumber ="v1.1 (2015-10-26)";
 		// versionNumber ="v1.2 (2016-03-10)";
-		versionNumber ="v1.3 (2018-05-03) Loop";
+		versionNumber ="v1.3 (2018-06-08) Loop";
 		
 		pathMain = "";
 		path1 = "";
@@ -37,151 +42,174 @@ public class PictureFileCompare {
 	/////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) throws IOException
     {
-    	//Main directory HERE
-    	PictureFileCompare fileObject = new PictureFileCompare();
     	
-    	//Ask for Main directory input
-    	fileObject.inputDirectory();
-    	
-    	
-    	//Set Up Log File
-    	//Check to see if log exists, if it uses that file, if it dose goes to the next number
-    	for (int c = 1; c < 100; c++)
-        {
-    		//starts stepping though files until it finds one that dose not exist
-    		fileObject.logfile = new File(fileObject.pathMain+"\\Log" + c + ".txt");
-    		boolean exists = fileObject.logfile.exists();
-    		
-    		//check to see if file does not exist, if so breaks the loop
-    		if (!exists){
-    			c = 100;
-    		}
-        }
-    	
-    	//Set Up Log File, Format: output("your message!", System.out, printWriter)
-    	fileObject.logfile.getParentFile().mkdirs(); //Makes new Directory for file
-    	PrintWriter printWriter = new PrintWriter(fileObject.logfile); //Set steam to write to file
-    	
-    	//Sets up Log File header
-    	fileObject.logFileSetup(printWriter);
-    	
-    	//*//remove 1st "/" to Turn off File maker and sorter <<<<
-    	//Make New Folders in main directory if they are not there
-    	output("\n--- createFolder() ---\n\n", System.out, printWriter); //DebugCode
-    	createFolder(fileObject.path1,printWriter); // RAW folder
-    	createFolder(fileObject.path2,printWriter); // JPG folder
-    	createFolder(fileObject.path3,printWriter); // MOV folder
-    	output("----------------------\n\n", System.out, printWriter); //DebugCode
-    	
-    	//Sort all files into folders based on extension
-    	output("\n---- fileSorter() ----", System.out, printWriter); //DebugCode
-    	fileObject.fileSorter(printWriter);
-    	output("\n----------------------\n", System.out, printWriter); //DebugCode
-    	//*/
-    	
-    	
-    	//*//remove 1st "/" to Turn off matching tool <<<<
-        //File class
-        File folder1 = new File(fileObject.path1);
-        File folder2 = new File(fileObject.path2);
-
-        //it gets the list of files
-        File[] listOfFiles1 = folder1.listFiles(); 
-        File[] listOfFiles2 = folder2.listFiles(); 
-
-        //Store the file names as Strings
-        ArrayList<String> fileNames1 = new ArrayList<String>();
-        ArrayList<String> fileNames2 = new ArrayList<String>();
-
-        //get file names from first directory
-        for (int i = 0; i < listOfFiles1.length; i++) 
-        {
-
-            if (listOfFiles1[i].isFile()) 
-            {
-                fileNames1.add(listOfFiles1[i].getName());
-            }
-        }
-
-        //get file names from second directory
-        for (int i = 0; i < listOfFiles2.length; i++) 
-        {
-
-            if (listOfFiles2[i].isFile()) 
-            {
-                fileNames2.add(listOfFiles2[i].getName());
-            }
-        }
-       
-        //Strings and boolean to temporarily store FileNames in order to compare them
-        String tempFileName1 = ""; //temporarily FileName 1
-        String tempFileName2 = ""; //temporarily FileName 2
-        boolean foundFile = false; //If file is found, then True
-        
-        
-        //MAIN LOGIC LOOP that goes though and see if there is a matching file
-        output("\n\n----- Matching files -----\n", System.out, printWriter); //Debug
-        for (int i = 0; i < fileNames1.size(); i++)
-        {
-        	
-        	//Store each file name at each location to "tempFileName1" every loop store to the next one 
-        	tempFileName1 = fileNames1.get(i);
-        	
-        	//Remove the extension from the file name, put back in "tempFileName1"
-        	tempFileName1 = tempFileName1.substring(0, tempFileName1.lastIndexOf('.'));
-        	
-        	//DebugCode
-        	output("\n>> (i:" + i + ") 1st File Name: (" + tempFileName1 +")", System.out, printWriter); //Debug
-        	
-        	//make sure "foundFile" is set to false to start, only fining the matching file will set it true
-        	foundFile = false;
-        	
-        	//nested loop to check all names against the one just stored in "tempFileName1"
-        	for (int n = 0; n < fileNames2.size(); n++)
-            {
-        		tempFileName2 = ""; //clear tempFileName2
-        		tempFileName2 = fileNames2.get(n); // get the next file name in the "fileNames2" array
-        		tempFileName2 = tempFileName2.substring(0, tempFileName2.lastIndexOf('.')); //Remove the extension
-        		
-        		output("\n(n:" + n +") FileName1: [" + tempFileName1 + "], FileName2: ["+ tempFileName2 +"]", System.out, printWriter); //Debug
-        		
-        		
-        		//if find a matching file then will set "foundFile = true" & end loop
-        		if (tempFileName1.equals(tempFileName2) && !foundFile)
-        		{
-        			output(" (Found it!)\n", System.out, printWriter); //debug
-        			foundFile = true; //foundFile true
-        			n = fileNames2.size(); //ends the loop by max out "n" counter
-        		}
-             }
-        	
-        	
-        	//If the file is not found when compared, then move to new folder
-        	if (!foundFile)
-        	{
-        		output("\n\nFile Not Found: (" + fileNames1.get(i) + ")\n", System.out, printWriter); // debug
-        		
-        		//Name the Save File that will store files with no match
-        		String saveFile = "NoMatch";
-
-        		//Make New folder to move files that don't have a match
-        		createFolder(fileObject.path1 + "\\" + saveFile,printWriter);
-        		
-    			//DebugCode
-        		output("Original Location >>> " +  fileObject.path1 + "\\" + fileNames1.get(i), System.out, printWriter); //Debug
-        		output("\nMove Location    >>> " +  fileObject.path1 + "\\" + saveFile + "\\" + fileNames1.get(i)+"\n", System.out, printWriter); //Debug
-    			
-    			//Move File from main directory to new folder that was just made
-    			File fileToMove  = new File( fileObject.path1 + "\\" + fileNames1.get(i));
-    			fileToMove.renameTo(new File(fileObject.path1 + "\\" + saveFile + "\\" + fileNames1.get(i)));
-        	}
-        }
-        output("--------------------------\n", System.out, printWriter); //Debug
-        //*/
-        
-        //LogFile Close
-        printWriter.flush(); // Flush printWriter File
-        printWriter.close(); // Close printWriter File
+    	while (mainLoop)
+    	{
+	    	
+	    	//Main directory HERE
+	    	PictureFileCompare fileObject = new PictureFileCompare();
+	    	
+	    	//Ask for Main directory input
+	    	fileObject.inputDirectory();
+	    	
+	    	
+	    	//Set Up Log File
+	    	//Check to see if log exists, if it uses that file, if it does goes to the next number
+	    	for (int c = 1; c < 100; c++)
+	        {
+	    		//starts stepping though files until it finds one that does not exist
+	    		fileObject.logfile = new File(fileObject.pathMain+"\\Log" + c + ".txt");
+	    		boolean exists = fileObject.logfile.exists();
+	    		
+	    		//check to see if file does not exist, if so breaks the loop
+	    		if (!exists){
+	    			c = 100;
+	    		}
+	        }
+	    	
+	    	//Set Up Log File, Format: output("your message!", System.out, printWriter)
+	    	fileObject.logfile.getParentFile().mkdirs(); //Makes new Directory for file
+	    	PrintWriter printWriter = new PrintWriter(fileObject.logfile); //Set steam to write to file
+	    	
+	    	//Sets up Log File header
+	    	fileObject.logFileSetup(printWriter);
+	    	
+	    	//*//remove 1st "/" to Turn off File maker and sorter <<<<
+	    	//Make New Folders in main directory if they are not there
+	    	output("\n--- createFolder() ---\n\n", System.out, printWriter); //DebugCode
+	    	createFolder(fileObject.path1,printWriter); // RAW folder
+	    	createFolder(fileObject.path2,printWriter); // JPG folder
+	    	createFolder(fileObject.path3,printWriter); // MOV folder
+	    	output("----------------------\n\n", System.out, printWriter); //DebugCode
+	    	
+	    	//Sort all files into folders based on extension
+	    	output("\n---- fileSorter() ----", System.out, printWriter); //DebugCode
+	    	fileObject.fileSorter(printWriter);
+	    	output("\n----------------------\n", System.out, printWriter); //DebugCode
+	    	//*/
+	    	
+	    	
+	    	//*//remove 1st "/" to Turn off matching tool <<<<
+	        //File class
+	        File folder1 = new File(fileObject.path1);
+	        File folder2 = new File(fileObject.path2);
+	
+	        //it gets the list of files
+	        File[] listOfFiles1 = folder1.listFiles(); 
+	        File[] listOfFiles2 = folder2.listFiles(); 
+	
+	        //Store the file names as Strings
+	        ArrayList<String> fileNames1 = new ArrayList<String>();
+	        ArrayList<String> fileNames2 = new ArrayList<String>();
+	
+	        //get file names from first directory
+	        for (int i = 0; i < listOfFiles1.length; i++) 
+	        {
+	
+	            if (listOfFiles1[i].isFile()) 
+	            {
+	                fileNames1.add(listOfFiles1[i].getName());
+	            }
+	        }
+	
+	        //get file names from second directory
+	        for (int i = 0; i < listOfFiles2.length; i++) 
+	        {
+	
+	            if (listOfFiles2[i].isFile()) 
+	            {
+	                fileNames2.add(listOfFiles2[i].getName());
+	            }
+	        }
+	       
+	        //Strings and boolean to temporarily store FileNames in order to compare them
+	        String tempFileName1 = ""; //temporarily FileName 1
+	        String tempFileName2 = ""; //temporarily FileName 2
+	        boolean foundFile = false; //If file is found, then True
+	        
+	        
+	        //MAIN LOGIC LOOP that goes though and see if there is a matching file
+	        output("\n\n----- Matching files -----\n", System.out, printWriter); //Debug
+	        for (int i = 0; i < fileNames1.size(); i++)
+	        {
+	        	
+	        	//Store each file name at each location to "tempFileName1" every loop store to the next one 
+	        	tempFileName1 = fileNames1.get(i);
+	        	
+	        	//Remove the extension from the file name, put back in "tempFileName1"
+	        	tempFileName1 = tempFileName1.substring(0, tempFileName1.lastIndexOf('.'));
+	        	
+	        	//DebugCode
+	        	output("\n>> (i:" + i + ") 1st File Name: (" + tempFileName1 +")", System.out, printWriter); //Debug
+	        	
+	        	//make sure "foundFile" is set to false to start, only fining the matching file will set it true
+	        	foundFile = false;
+	        	
+	        	//nested loop to check all names against the one just stored in "tempFileName1"
+	        	for (int n = 0; n < fileNames2.size(); n++)
+	            {
+	        		tempFileName2 = ""; //clear tempFileName2
+	        		tempFileName2 = fileNames2.get(n); // get the next file name in the "fileNames2" array
+	        		tempFileName2 = tempFileName2.substring(0, tempFileName2.lastIndexOf('.')); //Remove the extension
+	        		
+	        		output("\n(n:" + n +") FileName1: [" + tempFileName1 + "], FileName2: ["+ tempFileName2 +"]", System.out, printWriter); //Debug
+	        		
+	        		
+	        		//if find a matching file then will set "foundFile = true" & end loop
+	        		if (tempFileName1.equals(tempFileName2) && !foundFile)
+	        		{
+	        			output(" (Found it!)\n", System.out, printWriter); //debug
+	        			foundFile = true; //foundFile true
+	        			n = fileNames2.size(); //ends the loop by max out "n" counter
+	        		}
+	             }
+	        	
+	        	
+	        	//If the file is not found when compared, then move to new folder
+	        	if (!foundFile)
+	        	{
+	        		output("\n\nFile Not Found: (" + fileNames1.get(i) + ")\n", System.out, printWriter); // debug
+	        		
+	        		//Name the Save File that will store files with no match
+	        		String saveFile = "NoMatch";
+	
+	        		//Make New folder to move files that don't have a match
+	        		createFolder(fileObject.path1 + "\\" + saveFile,printWriter);
+	        		
+	    			//DebugCode
+	        		output("Original Location >>> " +  fileObject.path1 + "\\" + fileNames1.get(i), System.out, printWriter); //Debug
+	        		output("\nMove Location    >>> " +  fileObject.path1 + "\\" + saveFile + "\\" + fileNames1.get(i)+"\n", System.out, printWriter); //Debug
+	    			
+	    			//Move File from main directory to new folder that was just made
+	    			File fileToMove  = new File( fileObject.path1 + "\\" + fileNames1.get(i));
+	    			fileToMove.renameTo(new File(fileObject.path1 + "\\" + saveFile + "\\" + fileNames1.get(i)));
+	        	}
+	        }
+	        output("--------------------------\n", System.out, printWriter); //Debug
+	        //*/
+	        
+	        //LogFile Close
+	        printWriter.flush(); // Flush printWriter File
+	        printWriter.close(); // Close printWriter File
+	        
+	        
+	        //Restart Program, Open Scan, Ask to restart, Yes or No
+	        System.out.print("Restart Program? (Yes/No): ");
+	        consolescan = new Scanner(System.in); // open a new Scanner name it "scan"
+	        tempString = consolescan.nextLine(); // set [object being acted on] = [what was input into scanner]
+	        
+	        //Check what was inputed
+	        if (tempString.equalsIgnoreCase("yes") || tempString.equalsIgnoreCase("y")) {
+	        	System.out.println("Yes");
+	        	fileObject = null; //Remove Object
+	        } else if (tempString.equalsIgnoreCase("no") || tempString.equalsIgnoreCase("n")) {
+	        	System.out.println("No");
+	        	mainLoop = false;
+	        } else {
+	        	System.out.println("N/A Input");
+	        }
+	        
+    	}//End of Main Loop
         
         System.out.print("\n\nFinished: Press enter to exit..");
         System.in.read();
